b1e79e277dba1965168684fdc62204ac9ec4cf4a
M3g4n-P107r0wsk1 <77406457+M3g4n-P107r0wsk1@users.noreply.github.com>
2022-03-11 13:56:25 -0500

    added all required activities added tiling background to activities started laying out activities

---
+++ app/src/main/java/com/lh1153866/mcserverstatus/MyServersActivity.kt
@@ -0,0 +1,11 @@
+package com.lh1153866.mcserverstatus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyServersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_servers)
+    }
+}
---
+++ app/src/main/java/com/lh1153866/mcserverstatus/ServerStatusActivity.kt
@@ -0,0 +1,11 @@
+package com.lh1153866.mcserverstatus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ServerStatusActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_server_status)
+    }
+}
